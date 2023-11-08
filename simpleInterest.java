eac715535ebdc17a7d3e58e6da94733404b1ff1e
Happycodez <128193355+Happycodez@users.noreply.github.com>
2023-11-08 15:41:26 +0200

    Update simpleInterest.java
    Update simpleInterest

--- simpleInterest.java
+++ simpleInterest.java
@@ -1,16 +1,17 @@
 public class simpleInterest {
-
+    
     public static double calculatingSI(double rate, double deposit, int year) {
         double simpleInterest = (rate * deposit * year);
         return simpleInterest;
     }
-
+//Function taht calculate compund interest
     public static void main(String[] a) {
         double interestrate = 0.05;
         double amountdeposited = 100000;
         int years = 5;
-
+//We pass arguments to the function called above 
         double solution = calculatingSI(interestrate, amountdeposited, years);
+//Output
         System.out.println("Simple Interest: " + solution + " frw.");
     }
 }
