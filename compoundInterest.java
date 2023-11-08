981cca377d200a49e600064e020f8cdc6b44fa11
Happycodez <128193355+Happycodez@users.noreply.github.com>
2023-11-08 15:36:59 +0200

    Update compoundInterest.java
    comments

--- compoundInterest.java
+++ compoundInterest.java
@@ -2,6 +2,7 @@
 
 public class compoundInterest {
 
+// The below Loan function calculate the simple interest. 
     class Loan {
         public static double getTotalAmount(double principal, double rate, int time, int n) {
             double r = rate / 100;
@@ -9,15 +10,15 @@ public class compoundInterest {
             return A;
         }
     }
-
+// The below function is the main one that call the above one to be executed
     public static void main(String[] args) {
         double principal = 500000;
         double rate = 18;
         int time = 3;
         int n = 12;
-
+// We pass values/arguments to the called function
         double totalAmount = Loan.getTotalAmount(principal, rate, time, n);
-
+// output
         System.out.printf("Amount paid after %d years: %.2f frw", time, totalAmount);
     }
 
